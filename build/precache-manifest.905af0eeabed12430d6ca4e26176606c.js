839c3b7cbcffe7402fa8fd8cc482a7ef460ffa08
Mikkochu <mikko.s.kilpelainen@gmail.com>
2019-07-22 15:02:58 +0300

    uibuild

---
+++ build/precache-manifest.905af0eeabed12430d6ca4e26176606c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226f77eea6824cd5b64edf66b1b8b583",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72dfef9bbf08b5b39c8c",
+    "url": "/static/css/main.b0a7673b.chunk.css"
+  },
+  {
+    "revision": "edb70b9760340e495c07",
+    "url": "/static/js/2.42c16a5d.chunk.js"
+  },
+  {
+    "revision": "72dfef9bbf08b5b39c8c",
+    "url": "/static/js/main.a0c106f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static("build"));
 
 morgan.token("data", request => {
   return JSON.stringify(request.body); //Muuttaa json-muotoon
