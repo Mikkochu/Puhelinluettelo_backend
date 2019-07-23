f68077531f9d18172ffb97c523e26603755fab9f
Mikkochu <mikko.s.kilpelainen@gmail.com>
2019-07-23 14:29:47 +0300

    Tehtävä 3.17 tehty

--- index.js
+++ index.js
@@ -112,6 +112,22 @@ app.post("/api/persons", (request, response) => {
     .catch(error => next(error));
 });
 
+//Päivitys
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON());
+    })
+    .catch(error => next(error));
+});
+
 // MIDDLEWARE
 
 const requestLogger = (request, response, next) => {
